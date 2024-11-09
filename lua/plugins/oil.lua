d83c8440a72978c153c740d9cbbfb4bb4ba3e91f
AND3SIL4 <afsilva6236@misena.edu.co>
2024-11-09 01:24:21 -0500

    update(plugin): Oil plugin & new(plugin): UI plugin added

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -27,7 +27,7 @@ return {
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
       -- Open parent directory in floating window
-      vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+      vim.keymap.set("n", "<leader>2", require("oil").toggle_float, { desc = "Open toggle directory" })
     end,
   },
 }
