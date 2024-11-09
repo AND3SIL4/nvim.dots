53c35f9dfc879aafc1d260f4cc60dff2ae1b254d
AND3SIL4 <afsilva6236@misena.edu.co>
2024-11-08 23:28:53 -0500

    update(plugin): Oil plugin updated

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,17 @@
+-- This file contains the configuration for the oil.nvim plugin in Neovim.
+
 return {
   {
+    -- Plugin: oil.nvim
+    -- URL: https://github.com/stevearc/oil.nvim
+    -- Description: A Neovim plugin for managing and navigating directories.
     "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = {
+      -- Plugin: nvim-web-devicons
+      -- URL: https://github.com/nvim-tree/nvim-web-devicons
+      -- Description: A Lua fork of vim-web-devicons for Neovim.
+      "nvim-tree/nvim-web-devicons",
+    },
     config = function()
       require("oil").setup({
         columns = { "icon" },
