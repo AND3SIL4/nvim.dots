d8b24540d1893f78521b469de19314be1aba3fb0
AND3SIL4 <afsilva6236@misena.edu.co>
2025-03-15 14:24:21 -0500

    add(debuggin): new pluggin for debuggin in python added

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,33 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap-python",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("dapui").setup()
+    require("dap-python").setup("uv")
+
+    -- DAP UI configuration
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    -- Create and set the keymaps
+    vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+  end,
+}
